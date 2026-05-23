7ca168003c264521239a32f4d7a60179e4a6b07d
Oscarmle06 <oscarmlaraelizondo@gmail.com>
2026-05-22 21:46:00 -0600

    Changes in personality

--- Game/src/game/evasivePersonality.js
+++ Game/src/game/evasivePersonality.js
@@ -2,7 +2,7 @@
 // This module defines the EvasivePersonality class, a type of AI personality for CPU karts that focuses on avoiding obstacles and other karts.
 // Oscar Lara, Emilio Lara, Aixa Mendoza, May 2026
 
-import { Personality } from "./personality.js";
+import { Personality } from "./Personality.js";
 export class EvasivePersonality extends Personality {
     constructor() {
         super("evasive", 15, 0.05);
--- Game/src/game/strategicPersonality.js
+++ Game/src/game/strategicPersonality.js
@@ -2,7 +2,7 @@
 // This module defines the StrategicPersonality class, a type of AI personality for CPU karts that focuses on using cards strategically.
 // Note that the actual strategic behavior is not implemented yet, but will be.
 
-import { Personality } from "./personality.js";
+import { Personality } from "./Personality.js";
 export class StrategicPersonality extends Personality {
     constructor() {
         super("strategic", 20, 0.3);
